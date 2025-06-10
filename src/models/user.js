aa293deb719834cb6fca6e4e2a29b7600889304f
Hiran  Tharinda <hiran@surge.global>
2025-06-10 17:14:44 +0100

    update user

--- src/models/user.js
+++ src/models/user.js
@@ -4,6 +4,13 @@ const userSchema = new mongoose.Schema({
   firstName: { type: String, required: false },
   lastName: { type: String, required: false },
   nickName: { type: String, required: false },
+  workingStyle: { type: String, required: false },
+  workingHours: { type: String, required: false },
+  householdChores: { type: Array, required: false },
+  profession: { type: String, required: false },
+  likeReminders: { type: Boolean, default: true },
+  likeNotifications: { type: Boolean, default: true },
+  isNewUser: { type: Boolean, default: true },
   email: { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
