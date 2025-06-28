6bc39eca2de01a3a32b5385b47acc94169dc7d49
Hiran  Tharinda <hiran@surge.global>
2025-06-28 14:07:46 +0100

    update context

--- src/models/context.js
+++ src/models/context.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const contextSchema = new mongoose.Schema({
   name: { type: String, required: true },
   icon: { type: String },
+  color: { type: String },
+  description: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 });
 
